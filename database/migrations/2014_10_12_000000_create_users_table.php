0fcfa9db62310e20766dde7efb874c626a970388
Azka Bariqlana <azkbrq@gmail.com>
2023-12-18 15:09:57 +0700

    add table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username', 100);
             $table->string('password');
-            $table->rememberToken();
+            $table->string('nis');
+            $table->string('class');
             $table->timestamps();
         });
     }
