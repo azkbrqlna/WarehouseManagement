4af48168e00d1f1e0a710d5339573824850d4991
Azka Bariqlana <azkbrq@gmail.com>
2023-12-18 15:47:28 +0700

    add Role Model and make Role Seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Role::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $data = [
+            'admin', 'user'
+        ];
+
+        foreach ($data as $value) {
+            Role::insert([
+                'name' => $value
+            ]);
+        }
+    }
+}
