fe79b21d5e55cc0e9f843f085057e49c07e0484c
chomsin <ahmadruddin17@gmail.com>
2023-12-29 14:22:29 +0700

    inisialization user and kelas

--- resources/js/Layouts/Sidebar.jsx
+++ resources/js/Layouts/Sidebar.jsx
@@ -1,8 +1,11 @@
 import SidebarButton from "@/Components/Fragments/SidebarButton";
-import { Link } from "@inertiajs/react";
+import { Link, router } from "@inertiajs/react";
 import { Archive, Gauge, SignOut, Users } from "@phosphor-icons/react";
 
 const Sidebar = () => {
+    const onLogOut = () => {
+        router.visit("/logout");
+    };
     return (
         <>
             <aside className="h-screen p-10 bg-white w-72 flex flex-col justify-between">
@@ -12,10 +15,13 @@ const Sidebar = () => {
                     <SidebarButton value="Users" icon={Users} />
                 </div>
                 <div>
-                    <Link className="flex items-center gap-5 px-5 py-2 rounded-lg transition-all duration-300 hover:bg-red-100 border text-red-500 border-red-500">
+                    <button
+                        onClick={onLogOut}
+                        className="flex w-full items-center gap-5 px-5 py-2 rounded-lg transition-all duration-300 hover:bg-red-100 border text-red-500 border-red-500"
+                    >
                         <SignOut size={24} />
                         <span className="text-xl">Logout</span>
-                    </Link>
+                    </button>
                 </div>
             </aside>
         </>
